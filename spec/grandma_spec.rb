71c8a1b8254d18ce61b826d6ee10eb8ec4c3e015
Christopher <christopher.petronella@gmail.com>
2020-11-13 21:22:28 -0500

    commit tree 46926938a714475b32922402c52240b5a004e582

--- spec/grandma_spec.rb
+++ spec/grandma_spec.rb
@@ -1,6 +1,6 @@
 describe '#speak_to_grandma' do
   it 'responds with I LOVE YOU TOO PUMPKIN! if you say I LOVE YOU GRANDMA!' do
-    expect(speak_to_grandma('I LOVE YOU GRANDMA!')).to eq 'I LOVE YOU TOO PUMPKIN!'
+   expect(speak_to_grandma('I LOVE YOU GRANDMA!')).to eq 'I LOVE YOU TOO PUMPKIN!'
   end
 
   it 'responds with HUH?! SPEAK UP, SONNY! unless you are shouting' do
@@ -8,7 +8,7 @@ describe '#speak_to_grandma' do
   end
 
   it 'responds with HUH?! SPEAK UP, SONNY! for a second time' do
-    expect(speak_to_grandma('Hi!')).to eq 'HUH?! SPEAK UP, SONNY!'
+   expect(speak_to_grandma('Hi!')).to eq 'HUH?! SPEAK UP, SONNY!'
   end
 
   it 'responds with NO, NOT SINCE 1938! when she can hear you' do
@@ -20,3 +20,4 @@ describe '#speak_to_grandma' do
   end
 
 end
+
